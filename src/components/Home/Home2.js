fc8712959f96346724640a2e7e4d8b5bbb47225d
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-04 22:10:42 +0500

    added a connect link to my linkin

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -66,7 +66,7 @@ function Home2() {
 					<Col md={12} className='home-about-social'>
 						<h1>FIND ME ON</h1>
 						<p>
-							Feel free to <span className='purple'>connect </span>with me
+							Feel free to <span className='purple'><a href='https://www.linkedin.com/in/muhammad-zahooruddin-nizamani-844a58274/' className='purple'> connect</a> </span>with me
 						</p>
 						<ul className='home-about-social-links'>
 							<li className='social-icons'>
