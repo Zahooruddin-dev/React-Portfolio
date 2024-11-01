f6694258cdf1c2a5e2628bcea52f7bf8b85d2544
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-11-01 13:54:42 +0500

    1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,73 +1,73 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-function noTwitter(){
-  alert("I don't have any public Twitter Accounts")
+	AiFillGithub,
+	AiOutlineTwitter,
+	AiFillInstagram,
+} from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+function noTwitter() {
+	alert("I don't have any public Twitter Accounts");
 }
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
-  return (
-    <Container fluid className="footer">
-      <Row>
-        <Col md="4" className="footer-copywright">
-          <h3>Developed by Muhammad Zahooruddin</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} MZ</h3>
-        </Col>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="https://github.com/Zahooruddin-dev"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                style={{ color: "white" }}
-                target=""
-                onClick={noTwitter} 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/zahooruddinmizuka/"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/zahooruddin18/"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
-  );
+	let date = new Date();
+	let year = date.getFullYear();
+	return (
+		<Container fluid className='footer'>
+			<Row>
+				<Col md='4' className='footer-copywright'>
+					<h3>Developed by Muhammad Zahooruddin</h3>
+				</Col>
+				<Col md='4' className='footer-copywright'>
+					<h3>Copyright © {year} MZ</h3>
+				</Col>
+				<Col md='4' className='footer-body'>
+					<ul className='footer-icons'>
+						<li className='social-icons'>
+							<a
+								href='https://github.com/Zahooruddin-dev'
+								style={{ color: 'white' }}
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<AiFillGithub />
+							</a>
+						</li>
+						<li className='social-icons'>
+							<a
+								style={{ color: 'white' }}
+								target=''
+								onClick={noTwitter}
+								rel='noopener noreferrer'
+							>
+								<AiOutlineTwitter />
+							</a>
+						</li>
+						<li className='social-icons'>
+							<a
+								href='https://www.linkedin.com/in/zahooruddinmizuka/'
+								style={{ color: 'white' }}
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<FaLinkedinIn />
+							</a>
+						</li>
+						<li className='social-icons'>
+							<a
+								href='https://www.instagram.com/zahooruddin18/'
+								style={{ color: 'white' }}
+								target='_blank'
+								rel='noopener noreferrer'
+							>
+								<AiFillInstagram />
+							</a>
+						</li>
+					</ul>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default Footer;
