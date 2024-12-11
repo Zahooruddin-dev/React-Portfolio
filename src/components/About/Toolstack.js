2cd346124d8366ffb5686ea1d98524e4e4f272ea
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-12-11 16:20:55 +0500

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import {
-  SiVisualstudiocode,
-  SiPostman,
-  SiSlack,
-  SiVercel,
-  SiMacos,
-  SiWindows,
-  SiUnity,
-  SiGithub,
-  SiChatbot,
-  SiScrimba,
-  SiStackoverflow,
-  SiGoogle,
-  SiMozilla,
-  SiGamedeveloper,
-  SiMdnwebdocs,
-  SiNetlify,
-  SiRender,
-  SiGmail
-} from "react-icons/si";
+	SiVisualstudiocode,
+	SiPostman,
+	SiSlack,
+	SiVercel,
+	SiMacos,
+	SiWindows,
+	SiUnity,
+	SiGithub,
+	SiChatbot,
+	SiScrimba,
+	SiStackoverflow,
+	SiGoogle,
+	SiMozilla,
+	SiGamedeveloper,
+	SiMdnwebdocs,
+	SiNetlify,
+	SiRender,
+	SiGmail,
+} from 'react-icons/si';
 
 function Toolstack() {
-  return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-     {/*  <Col xs={4} md={2} className="tech-icons">
+	return (
+		<Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
+			{/*  <Col xs={4} md={2} className="tech-icons">
         <SiWindows />
       </Col>
  
@@ -34,45 +34,44 @@ function Toolstack() {
             <Col xs={4} md={2} className="tech-icons">
         <SiNetlify />
       </Col>*/}
-      <Col xs={4} md={2} className="tech-icons">
-        <SiUnity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGamedeveloper />
-      </Col> 
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGithub />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNetlify />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiGoogle />
-      </Col>
-    
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRender />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMdnwebdocs />
-      </Col><Col xs={4} md={2} className="tech-icons">
-        <SiStackoverflow />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiScrimba />
-      </Col> <Col xs={4} md={2} className="tech-icons">
-        <SiChatbot />
-      </Col> 
-    <Col xs={4} md={2} className="tech-icons">
-        <SiGmail />
-      </Col>    
-      
-      
-    </Row>
-  );
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiUnity />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiGamedeveloper />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiGithub />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiVercel />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiNetlify />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiGoogle />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiRender />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiMdnwebdocs />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiStackoverflow />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiScrimba />
+			</Col>{' '}
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiChatbot />
+			</Col>
+			<Col xs={4} md={2} className='tech-icons'>
+				<SiGmail />
+			</Col>
+		</Row>
+	);
 }
 
 export default Toolstack;
