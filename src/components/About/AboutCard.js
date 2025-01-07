fdc21736a4d6327432728f42bb401d43d08db88c
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2025-01-08 00:42:49 +0500

    fix

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
 				<blockquote className='blockquote mb-0'>
 					<p style={{ textAlign: 'justify' }}>
 						Hi Everyone, I am{' '}
-						<span className='purple'>Muhammad Zahooruddin </span>
+						<span className='purple'> Zahooruddin Nizamani </span>
 						from <span className='purple'> Hyderabad, Pakistan.</span>
 						<br />
 						<br />I completed high school with a focus on Computer Science and
